1019bdac51bb7290f61a10f33dacee9807c5635a
Waasiq Masood <waasiqmasood@gmail.com>
2021-11-12 17:40:49 +0100

    Smile Detection Added
    Smile detection has been added.

--- Example Projects/VolumeHandControl.py
+++ Example Projects/VolumeHandControl.py
@@ -49,25 +49,25 @@ while True:
         #* Coordinations of 2 landmarks we want to work with and the center of the distance
         x1, y1 = landmarkList[4][1], landmarkList[4][2] 
         x2, y2 = landmarkList[8][1], landmarkList[8][2]
-        cx, cy = (x1 + x2) // 2, (y1 +y2 ) // 2 
+        cx, cy = (x1 + x2) // 2, (y1 +y2 ) // 2  #* cx and cy is the middle of the circle
 
         cv2.circle(img, (x1,y1), 15, (255,255,0), cv2.FILLED)
         cv2.circle(img, (x2,y2), 15, (255,255,0), cv2.FILLED)
         cv2.line(img, (x1,y1), (x2,y2), (255,255,0), 3)
         cv2.circle(img, (cx, cy), 10, (255,255,0), cv2.FILLED)
 
-        lenght = math.hypot(x2 - x1, y2 - y1)
+        length = math.hypot(x2 - x1, y2 - y1)
 
         # Hand range 50 - 300
         # Volume range -65 - 0 (with the part below, we can redefine the ranges with numpy function)
-        vol = np.interp(lenght, [50, 300], [minVol, maxVol])
-        volBar = np.interp(lenght, [50, 300], [400, 100])
-        volPercentage = np.interp(lenght, [50, 300], [0, 100])
-        print(int(lenght),vol)
+        vol = np.interp(length, [50, 300], [minVol, maxVol])
+        volBar = np.interp(length, [50, 300], [400, 100])
+        volPercentage = np.interp(length, [50, 300], [0, 100])
+        print(int(length),vol)
         volume.SetMasterVolumeLevel(vol, None)
 
         # now we will create the condition to control the distance changes
-        if lenght < 50: #TODO --> for 'good soup' project, we can go for under 15
+        if length < 50: #TODO --> for 'good soup' project, we can go for under 15
             cv2.circle(img, (cx, cy), 10, (255,0,255), cv2.FILLED)
 
     #* Percentage and range output
--- iCare Project/test1.py
+++ iCare Project/test1.py
@@ -1,16 +1,22 @@
 import cv2
 import mediapipe as mp
 import time
+import math
 from Modules.FaceMeshModule import FaceMeshDetector 
 
 #* Resolution settings
 wCam, hCam = 640, 480
-dim = (620, 900)
+
+#! Default Dimensions for Webcame: 1280 x 720
+#! Dimensions for Test Videos:  700 x 925
+dim = (1280, 720)
+#dim = (700,925)
+
 
 def main():
     #* Video capture settings
-    # cap = cv2.VideoCapture(0)
-    cap = cv2.VideoCapture('videos/2.mp4')
+    cap = cv2.VideoCapture(0)
+    #cap = cv2.VideoCapture('videos/2.mp4')
     cap.set(3, wCam)
     cap.set(4, hCam)
 
@@ -20,15 +26,29 @@ def main():
         success, img = cap.read()
         img, faces = detector.findFaceMesh(img)
         
-        idList1 = faces[0][216] #? testing coordinates 216-96
-        idList2 = faces[0][96]
-        # print("id: {} x: {} y: {}".format(idList1[0], idList1[1], idList1[2]))
-        cv2.circle(img, (idList1[1], idList1[2]), 3, (0,255,0), 2, cv2.FILLED)
-        cv2.circle(img, (idList2[1], idList2[2]), 3, (0,255,0), 2, cv2.FILLED)
+        left_idList1 = faces[0][216]
+        left_idList2 = faces[0][184]
+        right_idList1 = faces[0][436]
+        right_idList2 = faces[0][408]
 
-        #TODO --> get coordinates from the id
+        left_topX, left_topY  = left_idList1[1], left_idList1[2]
+        left_botX, left_botY  = left_idList2[1], left_idList2[2]
+        right_topX, right_topY = right_idList1[1], right_idList1[2]
+        right_botX, right_botY = right_idList2[1], right_idList2[2]
 
 
+        #* Drawing landmarks on the facemesh in green
+        cv2.circle(img, (left_idList1[1],  left_idList1[2]),  3, (0,255,0), 2,  cv2.FILLED)
+        cv2.circle(img, (left_idList2[1],  left_idList2[2]),  3, (0,255,0), 2,  cv2.FILLED)
+        cv2.circle(img, (right_idList1[1], right_idList1[2]), 3, (0,255,0), 2 , cv2.FILLED)
+        cv2.circle(img, (right_idList2[1], right_idList2[2]), 3, (0,255,0), 2 , cv2.FILLED)
+
+        leftHypotonus = math.hypot(left_topX - left_botX, left_topY - left_botY)
+        rightHypotonus = math.hypot(right_topX - right_botX, right_topY - right_botY)
+
+        #* This if loop detects the smile. Change the hardcoded values w.r.t the Z axis.
+        if ((leftHypotonus < 18.5) and (rightHypotonus < 18.5)):
+           cv2.putText(img, "Smile :)", (20, 70), cv2.FONT_HERSHEY_PLAIN, 5, (255,0,0), 3)
 
 
         #* Final image output
@@ -37,8 +57,6 @@ def main():
         # cv2.imshow('Image', img)
         if cv2.waitKey(20) & 0xFF == ord('q'):
             break
-
-
 
 if __name__ == '__main__':
     main()
